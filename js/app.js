3094f31086064124ff947e7772857dd1aae943ea
EremeevValery <veron.en55@gmail.com>
2016-09-19 00:14:33 +0700

    add smart-rendering news

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
+var my_news =   [
+        /*{
+                author: 'Саша   Печкин',
+                text:   'В  четверг,    четвертого  числа...'
+        },
+        {
+                author: 'Просто Вася',
+                text:   'Считаю,    что $   должен  стоить  35  рублей!'
+        },
+        {
+                author: 'Гость',
+                text:   'Бесплатно. Скачать.    Лучший  сайт    -   http://localhost:3000'
+        }*/
+];
 var News    =   React.createClass({
-        render: function()  {
-                return  (
-                        <div    className="news">
-                                К   сожалению,  новостей    нет.
-                        </div>
-                );
-        }
+        render: function()   {
+                var data = this.props.data;
+                var newsTemplate;
+                let dataLength = data.length;
+                if (dataLength > 0) {
+                    newsTemplate = data.map(function(item,index) {
+                        return  (
+                          <div key={index}>
+                            <p  className="news__author">{item.author}:</p>
+                            <p  className="news__text">{item.text}</p>
+                          </div>
+                        )
+                    })
+                }  
+                else {
+                    newsTemplate=<p>новостей нет!!!!</p>
+                }
+        console.log(newsTemplate);
+        return (
+            <div className="news">
+                {newsTemplate}
+                <strong> Вcего новостей = {data.length}</strong>
+            </div>
+        );
+    }
 });
 
 var Comments    =   React.createClass({
@@ -23,7 +55,7 @@ var   App =   React.createClass({
                 return  (
                         <div    className="app">
                                 Всем    привет, я   компонент   App!    Я   умею    отображать  новости.
-                            <News />
+                            <News data={my_news}/>
                             <Comments />
                         </div>
 
@@ -32,6 +64,6 @@ var   App =   React.createClass({
 });
 
 ReactDOM.render(
-        <App    />,
+        <App />,
         document.getElementById('root')
 );
