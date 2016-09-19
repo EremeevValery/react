bfaae8957b0327b8aa6c36463d7760277f109289
EremeevValery <veron.en55@gmail.com>
2016-09-20 00:37:37 +0700

    add rendering for bigText

--- js/app.js
+++ js/app.js
@@ -20,16 +20,19 @@ var Article =   React.createClass({
         propTypes:  {
                 data:   React.PropTypes.shape({
                         author: React.PropTypes.string.isRequired,
-                        text:   React.PropTypes.string.isRequired
+                        text:   React.PropTypes.string.isRequired,
+                        bigText:   React.PropTypes.string.isRequired
                 })
         },
         render: function()  {
                 var author  =   this.props.data.author;
                 var text    =   this.props.data.text;
+                var bigText =   this.props.data.bigText;
                 return  (
                         <div    className="article">
                                 <p  className="news__author">{author}:</p>
                                 <p  className="news__text">{text}</p>
+                                <p  className="news__text">{bigText}</p>
                         </div>
                 )
         }
