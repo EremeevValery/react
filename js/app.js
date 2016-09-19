294ff3d5f879bfe28df7f32cdefe1c52f45d7207
EremeevValery <veron.en55@gmail.com>
2016-09-19 23:58:06 +0700

    add style for pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var my_news =   [
-        {
+        /*{
                 author: 'Саша   Печкин',
                 text:   'В  четверг,    четвертого  числа...'
         },
@@ -10,8 +10,22 @@ var my_news =   [
         {
                 author: 'Гость',
                 text:   'Бесплатно. Скачать.    Лучший  сайт    -   http://localhost:3000'
-        }
+        }*/
 ];
+
+var Article =   React.createClass({
+        render: function()  {
+                var author  =   this.props.data.author;
+                var text    =   this.props.data.text;
+                return  (
+                        <div    className="article">
+                                <p  className="news__author">{author}:</p>
+                                <p  className="news__text">{text}</p>
+                        </div>
+                )
+        }
+});
+
 var News    =   React.createClass({
         render: function()   {
                 var data = this.props.data;
@@ -32,23 +46,10 @@ var News    =   React.createClass({
         return (
             <div className="news">
                 {newsTemplate}
-                <strong className={data.length>0 ? '':'none'}> Вcего новостей = {data.length}</strong>
+                <strong className={'news__count ' + (data.length>0 ? '':'none')}> Вcего новостей = {data.length}</strong>
             </div>
         );
     }
-});
-
-var Article =   React.createClass({
-        render: function()  {
-                var author  =   this.props.data.author;
-                var text    =   this.props.data.text;
-                return  (
-                        <div    className="article">
-                                <p  className="news__author">{author}:</p>
-                                <p  className="news__text">{text}</p>
-                        </div>
-                )
-        }
 });
 
 var   App =   React.createClass({
